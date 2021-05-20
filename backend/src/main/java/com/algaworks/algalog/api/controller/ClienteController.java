cd437b9b35d66433dc3952f1564d6f4d231434f7
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-05-19 21:29:31 -0400

    Validation

--- backend/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ backend/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -2,6 +2,8 @@ package com.algaworks.algalog.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -54,13 +56,13 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar(@RequestBody Cliente cliente) {
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
 	
 	@PutMapping("/{clienteId}")
 	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,
-			@RequestBody Cliente cliente) {
+			@Valid @RequestBody Cliente cliente) {
 		if (!clienteRepository.existsById(clienteId)) {//Não existe?
 			return ResponseEntity.notFound().build();//Retorna o cod 404
 		}
